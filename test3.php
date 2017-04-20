8b96791ac2f1ea179f534b777845ffca9dd7f903
alfonso.mazza <alfonso.mazza@minfin.fed.be>
2017-04-20 15:30:51 +0200

    exercice 3

---
+++ test3.php
@@ -0,0 +1,13 @@
+<?php
+
+$total=0;
+for ($i = 0 ; $i < 31 ; $i++) {
+    $total=$total+$i;
+}
+echo " somme = $total";
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
